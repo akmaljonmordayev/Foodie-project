0faf9a06a28a0565b19865388d6075a986e60190
jojojijo302 <MUSTANGUZB170@gmail.com>
2025-10-30 14:39:02 +0500

    asdasdfasdfsadfasdfasdf

--- src/components/Main/Section2/Section2.jsx
+++ src/components/Main/Section2/Section2.jsx
@@ -1,39 +1,39 @@
 import React from "react";
 import "./Section2.css";
-import  logo1 from "/public/images/pngwing 4 (1).png"
+import logo1 from "/public/images/pngwing 4 (1).png";
 function Section2() {
   return (
     <section>
       <div className="container">
         <div className="sect2">
-<p>Customer Favorites</p>
-    <h3>Popular Categories</h3>
+          <p>Customer Favorites</p>
+          <h3>Popular Categories</h3>
         </div>
-    <div className="sect-card">
-      <div className="card">
-<img src={logo1} alt="" />
-<h4>Main Dish</h4>
-<p>(86 dishes)</p>
-      </div>
+        <div className="sect-card">
+          <div className="card">
+            <img src={logo1} alt="" />
+            <h4>Main Dish</h4>
+            <p>(86 dishes)</p>
+          </div>
 
-        <div className="card">
-<img src={logo1} alt="" />
-<h4>Break Fast</h4>
-<p>(12 break fast)</p>
-      </div>
+          <div className="card">
+            <img src={logo1} alt="" />
+            <h4>Break Fast</h4>
+            <p>(12 break fast)</p>
+          </div>
 
-        <div className="card">
-<img src={logo1} alt="" />
-<h4>Dessert </h4>
-<p> (48 dessert)</p>
-      </div>
+          <div className="card">
+            <img src={logo1} alt="" />
+            <h4>Dessert </h4>
+            <p> (48 dessert)</p>
+          </div>
 
-       <div className="card">
-<img src={logo1} alt="" />
-<h4>Browse All </h4>
-<p> (255 Items)</p>
-      </div>
-    </div>
+          <div className="card">
+            <img src={logo1} alt="" />
+            <h4>Browse All </h4>
+            <p> (255 Items)</p>
+          </div>
+        </div>
       </div>
     </section>
   );
