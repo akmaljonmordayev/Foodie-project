2e0a6b5468ba8836f747f370ba8422289a19a496
jojojijo302 <MUSTANGUZB170@gmail.com>
2025-10-30 14:39:58 +0500

    asdvasd

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,54 @@ function Footer() {
   return (
     <footer>
       <div className="container">
-        <div>Footer</div>
+        <div>
+            <div className="footer-container">
+
+              <div className="footer-section">
+                <h2 className="logo"><span className="logo-iconca">F</span>OODI</h2>
+                <p>Savor the artistry where every dish is a culinary masterpiece</p>
+                <div className="iconca">
+                  <a href="#"><i class="fab fa-facebook-f"></i></a>
+                  <a href="#"><i class="fab fa-instagram"></i></a>
+                  <a href="#"><i class="fab fa-twitter"></i></a>
+                  <a href="#"><i class="fab fa-youtube"></i></a>
+                </div>
+              </div>
+
+              <div className="footer-section linkaa">
+                <h3>Useful links</h3>
+                <ul>
+                  <li><a href="#">About us</a></li>
+                  <li><a href="#">Events</a></li>
+                  <li><a href="#">Blogs</a></li>
+                  <li><a href="#">FAQ</a></li>
+                </ul>
+              </div>
+
+              <div className="footer-section menu">
+                <h3>Main Menu</h3>
+                <ul>
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">Offers</a></li>
+                  <li><a href="#">Menus</a></li>
+                  <li><a href="#">Reservation</a></li>
+                </ul>
+              </div>
+
+              <div className="footer-section contact">
+                <h3>Contact Us</h3>
+                <p>example@email.com</p>
+                <p>+64 958 248 966</p>
+                <p>Social media</p>
+              </div>
+
+            </div>
+
+            <div className="footerbtn">
+              <p>Copyright © 2023 Dscode | All rights reserved</p>
+            </div>
+
+        </div>
       </div>
     </footer>
   );
--- src/components/Main/Section3/Section3.jsx
+++ src/components/Main/Section3/Section3.jsx
@@ -1,11 +1,45 @@
 import React from "react";
 import "./Section3.css";
+import logo1 from "/public/images/logo1.png";
+import logo2 from "/public/images/logo2.png";
+import logo3 from "/public/images/logo3.png";
+
 function Section3() {
   return (
-    <section>
-      <div className="container">
-        <div>section3</div>
-      </div>
+    <section className="sect3">
+        <p className="qiz">Special Dishes</p>
+<div className="left-sect3">
+<h1>Standout Dishes From Our Menu</h1>
+</div>
+<div className="cards">
+   <div className="card-food">
+    <img src={logo1} alt="" />
+    <p className="op">Fattoush salad</p>
+    <p>Description of the item</p>
+    <div className="star">
+<p className="yu" >$24.00</p>
+<p> ✨ 4.9</p>
+    </div>
+  </div>
+  <div className="card-food">
+    <img src={logo2} alt="" />
+    <p className="op" >Vegetable salad</p>
+    <p>Description of the item</p>
+    <div className="star">
+<p className="yu" >$26.00</p>
+<p> ✨ 4.6</p>
+    </div>
+  </div>
+  <div className="card-food">
+    <img src={logo3} alt="" />
+    <p className="op" >Egg vegi salad</p>
+    <p>Description of the item</p>
+    <div className="star">
+<p className="yu">$23.00</p>
+<p> ✨ 4.5</p>
+    </div>
+  </div>
+</div>
     </section>
   );
 }
