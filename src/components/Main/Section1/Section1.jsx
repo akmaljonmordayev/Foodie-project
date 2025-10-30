34adce93c3c9209dbb28322218bcdfdb497985ef
Kamol-F2540 <kamolbahodirov92@gmail.com>
2025-10-30 19:22:50 +0500

    done by Kamol

--- src/components/Main/Section1/Section1.jsx
+++ src/components/Main/Section1/Section1.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import "./Section1.css";
+import img from "/public/images/girl.png";
 function Section1() {
   return (
     <section>
       <div className="container">
-        <div>section1</div>
+        <div className="section">
+          <div className="section-left">
+            <p className="p1">Dive into Delights Of Delectable <span className="span1">Food</span></p>
+            <p className="p2">
+              Where Each Plate Weaves a Story of Culinary Mastery and Passionate
+              Craftsmanship
+            </p>
+            <button className="button1">Order Now</button>
+            <button className="button2">Watch Video</button>
+            <button className="button3">▶</button>
+          </div>
+          <div className="section-right">
+            <img className="img" src={img} alt="" />
+          </div>
+        </div>
       </div>
     </section>
   );
